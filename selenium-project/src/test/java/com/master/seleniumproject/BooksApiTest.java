1dbd6b7ad80d8edbf39d4d369109f6656237fc59
Volodymyr Chubenko <forelock7@gmail.com>
2024-10-27 11:22:09 +0200

    [Test] Add Delete API test to selenium (#25)

--- selenium-project/src/test/java/com/master/seleniumproject/BooksApiTest.java
+++ selenium-project/src/test/java/com/master/seleniumproject/BooksApiTest.java
@@ -14,44 +14,60 @@ import static com.master.seleniumproject.config.EnvConfigs.USERNAME;
 public class BooksApiTest {
     private BooksApiSteps booksApiSteps;
     private UserContext userContext;
-    private Book createBook;
-    private Book updateBook;
+    private Book bookToCreate;
+    private Book bookToUpdate;
     private Book newlyUpdatedBook;
+    private Book bookToDelete;
 
     @BeforeMethod
     public void setUp(ITestResult result) {
         booksApiSteps = new BooksApiSteps();
         userContext = new UserContext(USERNAME, PASSWORD);
         if ("createBook".equals(result.getMethod().getMethodName())) {
-            createBook = new Book("Learning Algorithms: A Programmer's Guide to Writing Better Code.", "George Heineman", "Study", 2021);
+            bookToCreate = new Book("Learning Algorithms: A Programmer's Guide to Writing Better Code.", "George Heineman", "Study", 2021);
         }
         if ("updateBook".equals(result.getMethod().getMethodName())) {
-            updateBook = new Book("Modernizing Enterprise Java", "Markus Eisele, Natale Vinto", "Study", 2023);
-            booksApiSteps.createBook(userContext, updateBook);
+            bookToUpdate = new Book("Modernizing Enterprise Java", "Markus Eisele, Natale Vinto", "Study", 2023);
+            booksApiSteps.createBook(userContext, bookToUpdate);
+        }
+        if ("deleteBook".equals(result.getMethod().getMethodName())) {
+            bookToDelete = new Book("Hands-On Selenium WebDriver with Java", "Boni Garcia", "Study", 2022);
+            booksApiSteps.createBook(userContext, bookToDelete);
         }
     }
 
     @AfterMethod
     public void tearDown(ITestResult result) {
         if ("createBook".equals(result.getMethod().getMethodName())) {
-            booksApiSteps.deleteBookByTitle(userContext, createBook.getTitle());
+            booksApiSteps.deleteBookByTitle(userContext, bookToCreate.getTitle());
         }
         if ("updateBook".equals(result.getMethod().getMethodName())) {
-            booksApiSteps.deleteBookByTitle(userContext, updateBook.getTitle());
+            booksApiSteps.deleteBookByTitle(userContext, bookToUpdate.getTitle());
+        }
+        if ("deleteBook".equals(result.getMethod().getMethodName())) {
+            booksApiSteps.deleteBookByTitle(userContext, bookToDelete.getTitle());
         }
     }
 
     @Test
     public void createBook() {
-        booksApiSteps.createBook(userContext, createBook);
-        booksApiSteps.verifyBookIsPresent(userContext, createBook);
+        booksApiSteps.createBook(userContext, bookToCreate);
+        booksApiSteps.verifyBookIsPresent(userContext, bookToCreate);
     }
 
     @Test
     public void updateBook() throws Exception {
-        booksApiSteps.verifyBookIsPresent(userContext, updateBook);
+        booksApiSteps.verifyBookIsPresent(userContext, bookToUpdate);
         newlyUpdatedBook = new Book("Modernizing Enterprise Java", "UPDATEDMarkus Eisele, Natale Vinto", "Study", 2023);
         booksApiSteps.updateBook(userContext, newlyUpdatedBook);
         booksApiSteps.verifyBookIsPresent(userContext, newlyUpdatedBook);
+    }
+
+    @Test
+    public void deleteBook() {
+        booksApiSteps.verifyBookIsPresent(userContext, bookToDelete);
+        booksApiSteps.deleteBookByTitle(userContext, bookToDelete.getTitle());
+
+        booksApiSteps.verifyBookIsAbsent(userContext, bookToDelete);
     }
 }
