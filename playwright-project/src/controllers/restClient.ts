5a620830c072cde3ac2ab1b2643333b5543494da
Volodymyr Chubenko <forelock7@gmail.com>
2024-10-28 23:13:42 +0200

    [Test] Add Delete UI test to playwright

--- playwright-project/src/controllers/restClient.ts
+++ playwright-project/src/controllers/restClient.ts
@@ -1,7 +1,7 @@
 // Creates initial context based on URL
-import {APIRequestContext, APIResponse, expect, request} from '@playwright/test';
-import {UserContext} from '@config/userContext';
-import {Login} from './login';
+import { APIRequestContext, APIResponse, expect, request } from '@playwright/test';
+import { UserContext } from '@config/userContext';
+import { Login } from './login';
 
 // Creates initial context based on URL
 export const getInitContext = async (baseURL: string) => {
@@ -14,16 +14,7 @@ export const getInitContext = async (baseURL: string) => {
 const getAccessToken = async (userContext: UserContext) => {
     const initContext: APIRequestContext = await getInitContext(userContext.baseUrl);
     const res: APIResponse = await Login.postLogin(userContext, initContext);
-
     expect(res.status()).toBe(200);
-    // const { access_token, success } = await res.json();
-    //
-    // expect(success).toBe(true);
-    //
-    // console.log(`Authorized by '${userContext.email}' with new access_token`);
-    // // Caches token for user context
-    // userContext.token = access_token;
-    // return access_token;
     return '';
 };
 
@@ -31,13 +22,13 @@ const getAccessToken = async (userContext: UserContext) => {
 export const getAuthorizedContext = async (
     userContext: UserContext,
 ): Promise<APIRequestContext> => {
-    const accessToken = userContext.token ?? (await getAccessToken(userContext));
-    // let extraHTTPHeaders: { Authorization: string } = {
-    //     Authorization: `Bearer ${accessToken}`,
-    // };
+    const accessToken = userContext.token; //?? (await getAccessToken(userContext));
+    let extraHTTPHeaders: { Authorization: string } = {
+        Authorization: `Bearer ${accessToken}`,
+    };
     return request.newContext({
         baseURL: userContext.baseUrl,
-        // extraHTTPHeaders,
+        extraHTTPHeaders,
         timeout: 30 * 1000,
     });
 };
